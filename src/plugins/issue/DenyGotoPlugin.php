9dcae84249c5101aa361d8951a8c8459a41ff423
algo13 <algo13.net@gmail.com>
2017-01-20 23:46:11 +0900

    Added DenyGotoPlugin

---
+++ src/plugins/issue/DenyGotoPlugin.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+use ast\Node;
+use Phan\{CodeBase, Plugin};
+use Phan\Plugin\PluginImplementation;
+use Phan\AST\AnalysisVisitor;
+use Phan\Language\Context;
+
+// PhanPluginDenyGoto
+return new class extends PluginImplementation
+{
+    public function analyzeNode(CodeBase $code_base, Context $context, Node $node, Node $parent_node = null)
+    {
+        (new class ($code_base, $context, $this) extends AnalysisVisitor
+        {
+            public function __construct(CodeBase $code_base, Context $context, Plugin $plugin)
+            {
+                parent::__construct($code_base, $context);
+                $this->plugin = $plugin;
+            }
+            public function visit(Node $node)
+            {
+            }
+            public function visitGoto(Node $node)
+            {
+                $this->plugin->emitIssue($this->code_base, $this->context, 'PhanPluginDenyGoto', 'goto operator are not allowed.');
+            }
+            /** @var Plugin */
+            private $plugin;
+        })($node);
+    }
+};
